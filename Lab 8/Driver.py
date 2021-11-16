52b366d8d2542092fca4e9e63f3b75b281e065b4
Nedwards0 <nedwards0001@gmai.com>
2021-11-15 17:14:46 -0800

    Update Driver.py

--- Lab 8/Driver.py
+++ Lab 8/Driver.py
@@ -39,9 +39,10 @@ class Enroll(db.Model):
 
 
 
-@app.route('/')
-def hello_world():
-    return render_template('login.html')
+@app.route('/login',methods=['GET','POST'])
+def login():
+    return render_template("login.html")
+    
 
 if __name__ == '__main__':
     app.debug = True
